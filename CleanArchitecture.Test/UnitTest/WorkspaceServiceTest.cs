fcbe32f72d62134bde46e84700a8330c1df3c0b4
Eng.EslamAhmed <ea495346@gmail.com>
2025-08-05 19:11:25 +0300

    complete the workspace test

--- CleanArchitecture.Test/UnitTest/WorkspaceServiceTest.cs
+++ CleanArchitecture.Test/UnitTest/WorkspaceServiceTest.cs
@@ -1,21 +1,24 @@
 ﻿using CleanArchitecture.API.Controllers;
 using CleanArchitecture.Application.DTOs.WorkspaceDtos;
 using CleanArchitecture.Application.Interfaces.Services;
+using CleanArchitecture.Domain.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
-
+using ThreadTask = System.Threading.Tasks.Task;
 
 namespace CleanArchitecture.Test.UnitTest;
 
 [TestFixture]
+[Category("Workspace")]
 public class WorkspaceServiceTest
 {
     private Mock<IWorkspaceService> _mockWorkspaceService;
     private Mock<IBaseServiceManager> _mockServiceManager;
     private WorkspaceController _controller;
 
-    [OneTimeSetUp]
+    [SetUp]
     public void SetUp()
     {
         _mockWorkspaceService = new Mock<IWorkspaceService>();
@@ -82,5 +85,197 @@ public class WorkspaceServiceTest
         Assert.That(result, Is.InstanceOf<NotFoundResult>());
     }
 
+    [Test]
+    public void GetWorkspaces_WhenExceptionThrown_ReturnsInternalServerError()
+    {
+        var exceptionMessage = "Database connection failed";
+
+        _mockServiceManager.Setup(workspace => workspace.WorkspaceService.GetAllWorkspaces())
+            .Throws(new Exception(exceptionMessage));
+
+        var result = _controller.GetWorkspaces();
+
+        Assert.That(result, Is.InstanceOf<ObjectResult>());
+
+        var objectResult = result as ObjectResult;
+        Assert.That(exceptionMessage, Is.EqualTo(objectResult?.Value));
+        Assert.That(StatusCodes.Status500InternalServerError, Is.EqualTo(objectResult?.StatusCode));
+    }
+
+    [Test]
+    public async ThreadTask CreateWorkspace_WithValidRequest_ReturnsCreated()
+    {
+        var workspace = new WorkspacesForCreationDto("Workspace 1", "workspace-1", "this is a workspace");
+
+        var workspaceDto = new WorkspaceDto(Guid.NewGuid(), "Workspace 1", "workspace-1", "this is a workspace", "i am the owner",DateTime.UtcNow);
+
+
+        _mockWorkspaceService.Setup(x => x.CreateWorkspace(workspace))
+            .ReturnsAsync(workspaceDto);
+
+        var result = await _controller.CreateWorkspace(workspace);
+        
+        Assert.That(result, Is.InstanceOf<CreatedAtActionResult>());
+
+        var objectResult = result as CreatedAtActionResult;
+
+        Assert.That(nameof(_controller.GetWorkspaceById), Is.EqualTo(objectResult?.ActionName));
+        Assert.That(workspaceDto.Id, Is.EqualTo(objectResult?.RouteValues?["id"]));
+        Assert.That(workspaceDto, Is.EqualTo(objectResult?.Value));
+        
+    }
+
+    [Test]
+    public async ThreadTask CreateWorkspace_WithInvalidModel_ReturnsBadRequest()
+    {
+        var request = new WorkspacesForCreationDto("","workspace-2","this is description");
+        _controller.ModelState.AddModelError("Name", "Name is required");
+
+        var result = await _controller.CreateWorkspace(request);
+
+        Assert.That(result, Is.InstanceOf<BadRequestObjectResult>());
+
+        var badRequestResult = result as BadRequestObjectResult;
+        Assert.That(StatusCodes.Status400BadRequest, Is.EqualTo(badRequestResult?.StatusCode));
+    }
+
+    [Test]
+    public async ThreadTask UpdateWorkspace_WithValidRequest_ReturnsNoContent()
+    {
+        var workspaceId = Guid.NewGuid();
+        var updatedWorkspace = new WorkspacesForUpdateDto("New workspace", "new-workspace", "");
+
+        _mockServiceManager.Setup(x => x.WorkspaceService.UpdateWorkspace(workspaceId, updatedWorkspace))
+            .ReturnsAsync(true);
+
+        var result = await _controller.UpdateWorkspace(workspaceId, updatedWorkspace);
+
+        Assert.That(result, Is.InstanceOf<NoContentResult>());
+    }
+
+    [Test]
+    public async ThreadTask UpdateWorkspace_WithInvalidModel_ReturnsBadRequest()
+    {
+        // Arrange
+        var workspaceId = Guid.NewGuid();
+
+        var request = new WorkspacesForUpdateDto(Name: "null",Slug: "",Description: "");
+
+        _controller.ModelState.AddModelError("Name", "Name is required");
+
+        // Act
+        var result = await _controller.UpdateWorkspace(workspaceId, request);
+
+        // Assert
+        Assert.That(result, Is.InstanceOf<BadRequestObjectResult>());
+    }
+
+    [Test]
+    public async ThreadTask UpdateWorkspace_WithNonExistingWorkspace_ReturnsNotFound()
+    {
+        // Arrange
+        var workspaceId = Guid.NewGuid();
+        var request = new WorkspacesForUpdateDto("workspace", "slug", "");
+        _mockWorkspaceService.Setup(x => x.UpdateWorkspace(workspaceId, request))
+            .ReturnsAsync(false);
+
+        // Act
+        var result = await _controller.UpdateWorkspace(workspaceId, request);
+
+        // Assert
+        Assert.That(result, Is.InstanceOf<NotFoundResult>());
+    }
+
+    [Test]
+    public async ThreadTask DeleteWorkspace_WithExistingId_ReturnsNoContent()
+    {
+        // Arrange
+        var workspaceId = Guid.NewGuid();
+        _mockWorkspaceService.Setup(x => x.DeleteWorkspace(workspaceId)).ReturnsAsync(true);
+
+        // Act
+        var result = await _controller.DeleteWorkspace(workspaceId);
+
+        // Assert
+        Assert.That(result,Is.InstanceOf<NoContentResult>());
+    }
+
+    [Test]
+    public async ThreadTask DeleteWorkspace_WithNonExistingId_ReturnsNotFound()
+    {
+        // Arrange
+        var workspaceId = Guid.NewGuid();
+        _mockWorkspaceService.Setup(x => x.DeleteWorkspace(workspaceId)).ReturnsAsync(false);
+
+        // Act
+        var result = await _controller.DeleteWorkspace(workspaceId);
+
+        // Assert
+        Assert.That(result, Is.InstanceOf<NotFoundResult>());
+    }
+
+    [Test]
+    public async ThreadTask DeleteWorkspace_WhenExceptionThrown_ReturnsInternalServerError()
+    {
+        // Arrange
+        var workspaceId = Guid.NewGuid();
+        var exceptionMessage = "Delete failed";
+        _mockWorkspaceService.Setup(x => x.DeleteWorkspace(workspaceId))
+            .ThrowsAsync(new Exception(exceptionMessage));
+
+        // Act
+        var result = await _controller.DeleteWorkspace(workspaceId);
+
+        // Assert
+        Assert.That(result, Is.InstanceOf<ObjectResult>());
+
+        var objectResult = result as ObjectResult;
+        Assert.That(StatusCodes.Status500InternalServerError,Is.EqualTo(objectResult?.StatusCode));
+        Assert.That(exceptionMessage, Is.EqualTo(objectResult?.Value));
+    }
+
+    [Test]
+    public void GetWorkspaceMembers_WhenCalled_ReturnsOkWithMembers()
+    {
+        var workspacedId = Guid.NewGuid();
+        var expectedMembers = new List<WorkspaceMemberDto>
+        {
+            new WorkspaceMemberDto { UserId = "user1",  FullName = "Name 1", Email = "nam1@gmail.com",Role ="Member",JoinedAt = DateTime.Now },
+            new WorkspaceMemberDto { UserId = "user2",  FullName = "Name 2", Email = "nam2@gmail.com",Role ="Admin",JoinedAt = DateTime.Now },
+        };
+
+        _mockServiceManager.Setup(x => x.WorkspaceService.WorkspaceMembers(workspacedId))
+            .Returns(expectedMembers);
+
+
+        var result = _controller.GetWorkspaceMembers(workspacedId);
+
+        Assert.That(result, Is.InstanceOf<OkObjectResult>());
+
+        var okObjectResult = result as OkObjectResult;
+
+        Assert.That(expectedMembers, Is.EqualTo(okObjectResult?.Value));
+    }
+
+    [Test]
+    public async ThreadTask AddUserToWorkspace_WithValidRequest_ReturnsOkWithUserWorkspace()
+    {
+        // Arrange
+        var request = new WorkspaceMemberForCreationDto(UserId: "user1", Guid.NewGuid(), Role: "member");
+        var expectedResult = new UserWorkspaceDto(UserId: "user1", Role: "member");
+
+        _mockWorkspaceService.Setup(x => x.AddToWorkspace(request))
+            .ReturnsAsync(expectedResult);
+
+        // Act
+        var result = await _controller.AddUserToWorkspace(request);
+
+        // Assert
+        Assert.That(result, Is.InstanceOf<OkObjectResult>());
+
+        var okResult = result as OkObjectResult;
+
+        Assert.That(expectedResult, Is.EqualTo(okResult?.Value));
+    }
 
 }
